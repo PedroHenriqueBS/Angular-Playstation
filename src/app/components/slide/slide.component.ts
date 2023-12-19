43bd0b481d34edc888a68a9de6392c461476207c
Pedro Henrique <pedrohenriqueb366@gmail.com>
2023-12-18 21:51:58 -0300

    aplicando responsividade 02

--- src/app/components/slide/slide.component.ts
+++ src/app/components/slide/slide.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-slide',
   templateUrl: './slide.component.html',
-  styleUrls: ['./slide.component.css']
+  styleUrls: ['./slide.component.css' , '../../../styles.css']
 })
 export class SlideComponent {
 
