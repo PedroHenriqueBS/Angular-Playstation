aa58eca624480159205dc9c8dc6b90300f9d4249
Pedro Henrique <pedrohenriqueb366@gmail.com>
2023-12-20 21:45:08 -0300

    aplicando responsividade 03

--- src/app/components/store/store.component.ts
+++ src/app/components/store/store.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-store',
   templateUrl: './store.component.html',
-  styleUrls: ['./store.component.css']
+  styleUrls: ['./store.component.css', '../../../styles.css']
 })
 export class StoreComponent {
 
